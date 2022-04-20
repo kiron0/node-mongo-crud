6f89122a216299efaaf37cab33379cadd3c13d17
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-20 23:26:00 +0600

    post method added

--- index.js
+++ index.js
@@ -2,21 +2,35 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // use middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://kiron0:Vks4Cj18OGXUI6SZ@kiron.ripcl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("foodMaster").collection("users");
-  console.log('db connected');
-
-  client.close();
+const uri =
+  "mongodb+srv://kiron0:Vks4Cj18OGXUI6SZ@kiron.ripcl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const usersCollection = client.db("foodMaster").collection("users");
+    app.post("/user", (req, res) => {
+      const newUser = req.body;
+      console.log("adding new user", newUser);
+      res.send("user data receive");
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Running my node CRUD Server");
