0c94b736afb26ea3d7cbf1b6e644c6f44c2da463
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-20 22:39:39 +0600

    mongodb added

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = "mongodb+srv://kiron0:Vks4Cj18OGXUI6SZ@kiron.ripcl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("foodMaster").collection("users");
+  console.log('db connected');
+
+  client.close();
+});
+
 
 app.get("/", (req, res) => {
   res.send("Running my node CRUD Server");
