47ba1e44e26ff098593186e0a9a6a03345ce9ebd
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-21 01:47:04 +0600

    finish

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
 
 // use middleware
 app.use(cors());
@@ -20,10 +21,54 @@ async function run() {
   try {
     await client.connect();
     const usersCollection = client.db("foodMaster").collection("users");
-    app.post("/user", (req, res) => {
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+
+    app.get("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/users", async (req, res) => {
       const newUser = req.body;
       console.log("adding new user", newUser);
-      res.send("user data receive");
+      const result = await usersCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    // update user
+    app.put("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedUser = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          name: updatedUser.name,
+          email: updatedUser.email,
+        },
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updatedDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    // delete a user
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
     // await client.close();
